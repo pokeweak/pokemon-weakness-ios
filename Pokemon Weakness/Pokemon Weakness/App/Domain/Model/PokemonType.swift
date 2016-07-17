462e63ebbe4dd184cc3fcd62dd856babed5d9eea
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-17 19:18:50 +0200

    Define enum with the available list of pokemon types

---
+++ Pokemon Weakness/Pokemon Weakness/App/Domain/Model/PokemonType.swift
@@ -0,0 +1,34 @@
+//
+//  PokemonType.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 17/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import Foundation
+
+/**
+ List of pokemon's types extracted from http://pokemondb.net/type
+ */
+enum PokemonType {
+    
+    case Normal
+    case Fire
+    case Water
+    case Electric
+    case Grass
+    case Ice
+    case Fighting
+    case Poison
+    case Ground
+    case Flying
+    case Psychic
+    case Bug
+    case Rock
+    case Ghost
+    case Dragon
+    case Dark
+    case Steel
+    case Fairy
+}
