c58c14e903705f9c26a6923aa7857aa2810a5240
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-17 19:21:14 +0200

    Define the pokemon domain model

---
+++ Pokemon Weakness/Pokemon Weakness/App/Domain/Model/Pokemon.swift
@@ -0,0 +1,16 @@
+//
+//  Pokemon.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 17/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+    
+    let name: String
+    let number: Int
+    let type: PokemonType
+}
