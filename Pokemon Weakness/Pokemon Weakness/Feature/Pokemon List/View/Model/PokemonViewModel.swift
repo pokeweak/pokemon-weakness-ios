adbdd4f60ce8ab43a52c20a628b1ea3fec09ea3a
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-17 21:29:07 +0200

    Define the pokemon view model

---
+++ Pokemon Weakness/Pokemon Weakness/Feature/Pokemon List/View/Model/PokemonViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonViewModel.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 17/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import UIKit
+
+struct PokemonViewModel {
+    
+    let name: String
+    let number: String
+    let image: UIImage
+    let color: UIColor
+    let weaknesses: [String]
+}
