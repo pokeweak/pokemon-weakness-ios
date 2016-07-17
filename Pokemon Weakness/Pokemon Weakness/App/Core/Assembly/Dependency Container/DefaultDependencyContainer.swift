601bb6dd3ab28712c03296da593bf6444f97f4b7
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-17 19:46:26 +0200

    Implement a basic dependency container by storing the instances into a dictionary

---
+++ Pokemon Weakness/Pokemon Weakness/App/Core/Assembly/Dependency Container/DefaultDependencyContainer.swift
@@ -0,0 +1,70 @@
+//
+//  DefaultDependencyContainer.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 17/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import Foundation
+
+private class DefaultDependencyContainer: DependencyContainer {
+    
+    private static let container = DefaultDependencyContainer()
+    private var serviceInstances = [String: AnyObject]()
+    
+    private init() {}
+    
+    func register(key: String, instance: AnyObject) {
+        serviceInstances[key] = instance
+    }
+    
+    func get(key: String) -> AnyObject? {
+        return serviceInstances[key]
+    }
+    
+    func remove(key: String) {
+        serviceInstances.removeValueForKey(key)
+    }
+    
+    func removeAll() {
+        serviceInstances.removeAll()
+    }
+}
+
+/**
+ Returns the default dependency container
+ */
+func Container() -> DependencyContainer {
+    return DefaultDependencyContainer.container
+}
+
+/**
+ Register and save instance of an object
+ 
+ - parameter service: service instance to register/locate
+ - parameter key:     an key identifier
+ 
+ - returns: returns a cached instance of the service
+ */
+func Register<T where T: AnyObject>(service: T, key: String) -> T {
+    
+    if let service = Container().get(key) {
+        assertionFailure("Dependency is already registered \(service) -> \(key)")
+    }
+    
+    DefaultDependencyContainer.container.register(key, instance: service)
+    
+    return service
+}
+
+/**
+ Check if an instance is already registered
+ 
+ - parameter key: instance key identifier
+ 
+ - returns: wether the instance is registered
+ */
+func Resolve<T where T: AnyObject>(key: String) -> T? {
+    return Container().get(key) as? T
+}
