d584637ae69f5f9f24f9998827b370e415272876
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-17 20:59:19 +0200

    Add the pokemon item cell with an inicial layout

---
+++ Pokemon Weakness/Pokemon Weakness/Feature/Pokemon List/View/Cell/PokemonItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonItemCell.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 17/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import UIKit
+
+class PokemonItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
