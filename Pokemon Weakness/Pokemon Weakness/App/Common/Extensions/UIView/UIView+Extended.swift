adb25fa548a679628241df6aa7f3335c6127d5a4
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-18 00:15:01 +0200

    Extends UIView with function for applying shadows with custom values

---
+++ Pokemon Weakness/Pokemon Weakness/App/Common/Extensions/UIView/UIView+Extended.swift
@@ -0,0 +1,40 @@
+//
+//  UIView+Extended.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 18/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import UIKit
+
+private let defaultShadowColor: UIColor = .blackColor()
+private let defaultShadowRadius: Float = 4.0
+private let defaultShadowOpacity: Float = 0.2
+private let defaultCornerRadius: Float = 0
+
+extension UIView {
+    
+    func applyShadow(shadowColor: UIColor = defaultShadowColor,
+                     shadowRadius: Float = defaultShadowRadius,
+                     shadowOpacity: Float = defaultShadowOpacity,
+                     cornerRadius: Float = defaultCornerRadius)
+    {
+        let shadowPath = UIBezierPath(
+            roundedRect: bounds,
+            cornerRadius: CGFloat(cornerRadius)
+            ).CGPath
+        
+        layer.masksToBounds = false
+        layer.shadowPath = shadowPath
+        layer.shadowColor = shadowColor.CGColor
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+        layer.shadowRadius = CGFloat(shadowRadius)
+        layer.shadowOpacity = shadowOpacity
+        layer.cornerRadius = CGFloat(cornerRadius)
+    }
+    
+    func applyCornerRadius(cornerRadius: Float = defaultCornerRadius) {
+        layer.cornerRadius = CGFloat(cornerRadius)
+    }
+}
