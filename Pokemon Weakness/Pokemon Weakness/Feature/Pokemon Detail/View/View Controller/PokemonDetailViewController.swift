c70134ea88a34c798f8acf41cac713f86ff280bc
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-24 22:41:11 +0200

    Add pokemon detail view controller

---
+++ Pokemon Weakness/Pokemon Weakness/Feature/Pokemon Detail/View/View Controller/PokemonDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 23/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
