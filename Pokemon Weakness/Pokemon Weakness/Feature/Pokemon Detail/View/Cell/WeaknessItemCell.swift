39203eb16cd332c8183bf122bba73afbbc17d97e
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-25 16:07:31 +0200

    Add weakness item cell

---
+++ Pokemon Weakness/Pokemon Weakness/Feature/Pokemon Detail/View/Cell/WeaknessItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeaknessItemCell.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 25/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import UIKit
+
+class WeaknessItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
