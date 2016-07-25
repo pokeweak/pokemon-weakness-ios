0a50dc942015874ba376caee23161d0b5a0b5e76
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-25 18:56:22 +0200

    no message

--- Pokemon Weakness/Pokemon Weakness/Feature/Pokemon List/View/Cell/Renderer/PokemonItemCellRenderer.swift
+++ Pokemon Weakness/Pokemon Weakness/Feature/Pokemon List/View/Cell/Renderer/PokemonItemCellRenderer.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct PokemonItemCellRenderer: Renderable {
+class PokemonItemCellRenderer: Renderable {
     
     func render(data: PokemonViewModel, into view: PokemonItemCellView) {
         
