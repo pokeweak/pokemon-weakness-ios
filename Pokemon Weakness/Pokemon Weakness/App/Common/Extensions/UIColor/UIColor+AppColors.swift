818f6638769fc25b9802c1a9a46b68314b02c3b4
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-26 17:32:31 +0200

    Define application style guid (font + colors)

--- Pokemon Weakness/Pokemon Weakness/App/Common/Extensions/UIColor/UIColor+AppColors.swift
+++ Pokemon Weakness/Pokemon Weakness/App/Common/Extensions/UIColor/UIColor+AppColors.swift
@@ -13,4 +13,12 @@ extension UIColor {
     public class func catskillWhite() -> UIColor {
         return UIColor(hex: 0xEDF3F7)
     }
+    
+    public class func blueBayoux() -> UIColor {
+        return UIColor(hex: 0x4D6478)
+    }
+    
+    public class func pickledBluewood() -> UIColor {
+        return UIColor(hex: 0x2A3C4B)
+    }
 }
