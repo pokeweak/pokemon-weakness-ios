e3a219e08550d51d857f41e6dcf8e2d54ef7f50c
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-29 15:33:46 +0200

    Add view model for the about items and the cell renderer

---
+++ Pokemon Weakness/Pokemon Weakness/Feature/About/Domain/Model/AboutItemViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AboutItemViewModel.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 29/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import Foundation
+
+struct AboutItemViewModel {
+    
+    let pokeball: String
+    let name: String
+    let job: String
+}
