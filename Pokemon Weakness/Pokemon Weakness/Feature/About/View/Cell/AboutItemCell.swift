9d1f47090794cdb42204de42d9188a1a9d605022
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-07-29 15:23:04 +0200

    Add layout for the about cell

---
+++ Pokemon Weakness/Pokemon Weakness/Feature/About/View/Cell/AboutItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  AboutItemCell.swift
+//  Pokemon Weakness
+//
+//  Created by Oscar Duran on 29/7/16.
+//  Copyright © 2016 Skydev. All rights reserved.
+//
+
+import UIKit
+
+class AboutItemCell: UICollectionViewCell {
+
+    @IBOutlet weak var pokeballImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
