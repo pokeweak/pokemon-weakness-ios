64e109f4a86e12069e9aa3730a658d1985e208ae
Oscar Antonio Duran Grillo <skyweb09@hotmail.es>
2016-08-02 17:23:05 +0200

    Add twitter account to the AboutViewModel

--- Pokemon Weakness/Pokemon Weakness/Feature/About/Presenter/AboutPresenter.swift
+++ Pokemon Weakness/Pokemon Weakness/Feature/About/Presenter/AboutPresenter.swift
@@ -21,9 +21,30 @@ class AboutPresenter {
     func viewDidLoad() {
         
         let team = [
-            AboutItemViewModel(pokeball: "pokeball_green", color: .greenishTeal(), name: "Alex Bailon", job: "Product Designer"),
-            AboutItemViewModel(pokeball: "pokeball_red", color: .tomato(), name: "Oscar Duran", job: "iOS Developer"),
-            AboutItemViewModel(pokeball: "pokeball_blue", color: .darkSkyBlue(), name: "Rafa Vázquez", job: "Android Developer")
+            
+            AboutItemViewModel(
+                pokeball: "pokeball_green",
+                color: .greenishTeal(),
+                name: "Alex Bailon",
+                job: "Product Designer",
+                twitter: NSURL(string: "https://twitter.com/Alex_Bailon")!
+            ),
+
+            AboutItemViewModel(
+                pokeball: "pokeball_red",
+                color: .tomato(),
+                name: "Oscar Duran",
+                job: "iOS Developer",
+                twitter: NSURL(string: "https://twitter.com/skyweb07")!
+            ),
+            
+            AboutItemViewModel(
+                pokeball: "pokeball_blue",
+                color: .darkSkyBlue(),
+                name: "Rafa Vázquez",
+                job: "Android Developer",
+                twitter: NSURL(string: "https://twitter.com/sloydev")!
+            )
         ]
         
         aboutCollectionDataSource.items = team
